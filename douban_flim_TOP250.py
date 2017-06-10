f14ff91ad194e2bba975b41a13f9ba4a747ddebb
cl1330104058 <lc1330104058@gmail.com>
2017-06-10 20:59:27 +0800

    爬取代理 IP

--- douban_flim_TOP250.py
+++ douban_flim_TOP250.py
@@ -12,9 +12,9 @@ from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
 # 创建数据库
-client = MongoClient('localhoat',27017)
-douban = client['douban']
-douban_flim_TOP250 = douban['douban_flim_TOP250']
+# client = MongoClient('localhoat',27017)
+# douban = client['douban']
+# douban_flim_TOP250 = douban['douban_flim_TOP250']
 
 headers = {
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 UBrowser/6.1.2716.203 Safari/537.36'
@@ -29,9 +29,11 @@ def get_movie(url):
         rank = item.em.text  # 得到电影排名
         titles = item.find_all('span',{'class':'title'})
         if len(titles) > 1:  # 得到电影的标题，用 if 判定电影是否有外国片名
-            title = titles[0].get_text() + titles[1].get_text()
+            name = titles[0].get_text()
+            name_en = titles[1].get_text().replace('\xa0',' ')
         else:
-            title = titles[0].get_text()
+            name = titles[0].get_text()
+            name_en = 'None'
         # 对 <div class=''bd>下的 <p> 标签切片，得到 电影的发行年代，国家，类型
         p = item.p.get_text().split('\n')[2]
         details = p.split('/')
@@ -39,19 +41,21 @@ def get_movie(url):
         country = details[1].strip().split(' ')
         movie_classes = details[2].replace('\xa0','').split(' ')
         # 得到评分，引用的评论
-        rating = item.find('span',{'class':'rating_num'})
-        cate = item .find('span',{'class':'inq'})
+        rating = item.find('span',{'class':'rating_num'}).get_text()
+        cate = item .find('span',{'class':'inq'}).get_text()
         # 将得到的数据写进字典，以便存入数据库
         data = {
             'rank':rank,
-            'title':title.replace('\xa0',' '),
-            'rating': rating.get_text(),
+            'name':name,
+            'name_en':name_en,
+            'rating': rating,
             'pub_time':pub_time,
             'country':country,
             'classes':movie_classes,
-            'cate':cate.get_text()
+            'cate':cate
         }
-        douban_flim_TOP250.insert_one(data)
+        print(data)
+        #douban_flim_TOP250.insert_one(data)
 
 
 urls = ['https://movie.douban.com/top250?start={}'.format(n) for n in range(0,250,25)]
---
+++ ip_proxy.py
@@ -0,0 +1,60 @@
+import time
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+headers = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 UBrowser/6.1.2716.203 Safari/537.36'
+}
+
+# 存储最终结果
+result = []
+
+def vf(pool,ip):
+    try:
+        response = requests.get('http://www.baidu.com', headers=headers, proxies={'http':ip}, timeout=3)
+    except:
+        pass
+    else:
+        pool.append(ip)
+
+# 爬取 cybersyndrome
+def get_cyb(result):
+    browser = webdriver.PhantomJS(executable_path='D:\PhantomJS')
+    browser.get('http://www.cybersyndrome.net/pla6.html')
+    time.sleep(5)
+    ip_a = browser.find_elements_by_class_name('A')
+    ip_b = browser.find_elements_by_class_name('B')
+    ip_c = browser.find_elements_by_class_name('C')
+    address = []
+    for ip in ip_a:
+        http_a = 'http://' + ip.text
+        address.append(http_a)
+    for ip in ip_b:
+        http_b = 'http://' + ip.text
+        address.append(http_b)
+    for ip in ip_c:
+        http_c = 'http://' + ip.text
+        address.append(http_c)
+    for ip in address:
+        vf(result,ip)
+
+# 爬取西刺代理
+def get_xicidaili(ip_list):
+    urls = ['http://www.xicidaili.com/nn/{}'.format(n) for n in range(1,11)]
+    for url in urls:
+        # 其实每次抓取页面之间应该有一定的时间间隔的，但是由于要验证IP花费很多时间，就省去了这个过程
+        page_data = requests.get(url,headers=headers)
+        soup = BeautifulSoup(page_data.text,'lxml')
+        items = soup.find_all('tr')
+        items.pop(0)
+        for item in items:
+            ip = item.select('td:nth-of-type(2)')[0].get_text()
+            port = item.select('td:nth-of-type(3)')[0].get_text()
+            http = 'http://' + ip + ':' + port
+            vf(ip_list,http)
+
+get_cyb(result)
+get_xicidaili(result)
+
+print(result)
