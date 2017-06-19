5f25e62587ca1afb6b366e21fe57ac471cee3416
cl1330104058 <lc1330104058@gmail.com>
2017-06-19 21:44:33 +0800

    新增影片磁力

--- douban_file_week.py
+++ douban_file_week.py
@@ -19,20 +19,39 @@ def douban_xinpian():
     items = soup.find_all('div',{'class':'pl2'})
     for item in items:
         title = item.find('a').get_text().replace('\n','').replace(' ','')
+        search = title.split('/')[0]
         rate = item.find('span',{'class':'rating_nums'}).get_text()
         link = item.find('a')['href']
         print(title,rate,link)
+        get_cili(search)
 
 def douban_week():
     time = soup.find('span',{'class':'box_chart_num'}).get_text()
     print('时间：',time)
     items = soup.find_all('div',{'class':'name'})
     for item in items:
-        NO = items.index(item) + 1
+        No = items.index(item) + 1
         title = item.find('a').get_text().replace('\n','').replace(' ','')
+        search = title.split('/')[0]
         link = item.find('a')['href']
-        print(NO,title,link)
+        print(No,title,link)
+        get_cili(search)
+
+def get_cili(title):
+    url = 'http://www.diaosisou.net/list/{}/1'.format(title)
+    page_data = requests.get(url,headers=headers)
+    soup = BeautifulSoup(page_data.text,'lxml')
+    items = soup.find_all('div',{'class':'dInfo'})
+    if len(items) == 0:
+        print('找不到磁力链接')
+    else:
+        ls = []
+        for item in items[0:5]:
+            magnet = item.find('a')['href']
+            ls.append(magnet)
+        print(ls)
 
 douban_xinpian()
 print('-------------------------')
 douban_week()
+
